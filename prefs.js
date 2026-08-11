a55b38309a510297394aae709086afa62b296029
jamir-boop <jeiserelejamir@gmail.com>
2026-08-11 15:45:27 -0500

    fix(prefs): remove GI import versions

--- prefs.js
+++ prefs.js
@@ -1,7 +1,7 @@
 import Adw from 'gi://Adw';
-import Gdk from 'gi://Gdk?version=4.0';
+import Gdk from 'gi://Gdk';
 import Gio from 'gi://Gio';
-import Gtk from 'gi://Gtk?version=4.0';
+import Gtk from 'gi://Gtk';
 
 import {ExtensionPreferences} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
